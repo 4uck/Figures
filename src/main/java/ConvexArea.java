1875c7989653e3b1e6f8b07fdeff2780f23168f9
Dima Monin <monin.mitya@mail.ru>
2018-07-24 09:24:03 +0700

    first commit

---
+++ src/main/java/ConvexArea.java
@@ -0,0 +1,46 @@
+
+public class ConvexArea {
+
+    private Coordinate vertex0;
+    private Coordinate vertex1;
+    private Coordinate vertex3;
+
+    private int tg;
+
+    public ConvexArea(Coordinate vertex0, Coordinate vertex1) {
+        this.vertex0 = vertex0;
+        this.vertex1 = vertex1;
+        this.vertex3 = new Coordinate(vertex0.getX(), vertex1.getY());
+        setTg();
+    }
+
+    public Coordinate getVertex0() {
+        return vertex0;
+    }
+
+    public Coordinate getVertex1() {
+        return vertex1;
+    }
+
+    public Coordinate getVertex3() {
+        return vertex3;
+    }
+
+    private void setTg(){
+        int a = vertex0.getY() - vertex3.getY();
+        int b = vertex1.getX() - vertex3.getX();
+
+        this.tg = b/a;
+    }
+
+    public boolean isAccessMove(Coordinate point, int move){
+
+        int a_mod = vertex0.getY() - point.getY();
+        int b_mod = tg * a_mod;
+
+        if (vertex0.getX() + b_mod <= point.getX() + move)
+            return false;
+
+        return true;
+    }
+}
---
+++ src/main/java/Solution.java
@@ -0,0 +1,14 @@
+
+public class Solution {
+
+    public void algorithm_1(
+            ConvexArea convexArea,
+            Coordinate point0,
+            Coordinate point1
+    ){
+        int directionX = point0.getX() > point1.getX() ? -1 : 1;
+        int directionY = point0.getY() > point1.getY() ? -1 : 1;
+
+
+    }
+}
